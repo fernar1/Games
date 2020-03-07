1ebb061fe0e07dbe0fac119bc7897b7f2fdcf1a0
Roland Fernandes <roland.fernandes@gmail.com>
2020-03-06 22:25:43 -0500

    Initial checkin for the main.py

---
+++ rps/main.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb 23 14:18:56 2020
+@author: fernar1
+@desc: code for classsic rock paper scissors game
+"""
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
+    
+

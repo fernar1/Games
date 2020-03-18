ec1f37d068b5c5f65961ba6f8bfc447af9f9d9ae
Roland Fernandes <roland.fernandes@gmail.com>
2020-03-18 16:57:46 -0700

    Moved the logic to a seperate class from main function

--- hangman/main.py
+++ hangman/main.py
@@ -8,42 +8,86 @@ Created on Sun Mar 15 14:24:14 2020
 
 import random
 
-words = ['abruptly', 'absurd', 'abyss', 'affix', 'avenue']
+class hangman():
+    _words = ['abruptly', 'absurd', 'abyss', 'affix', 'avenue']
+    
+    
+    def __init__(self):
+        self._word = self._words[random.randint(0, 4)]
+        self._num_guess = 6
+        self._lst_guess = ['_' ] * len(self._word)
+    
+    @property
+    def guess_word_len(self):
+        return len(self._word)
+    
+    @property
+    def guess_word(self):
+        return self._word
+    
+    @property
+    def num_guess(self):
+        return self._num_guess
+
+    @num_guess.setter
+    def num_guess(self, num_guess):
+        self._num_guess = num_guess
+    
+    @property
+    def lst_guess(self):
+        return self._lst_guess
+    
+    def guess_remain(self):
+        return self._num_guess > 0
+    
+    def process_guess(self, guess_char):
+        match_found = True
+        if not guess_char.isalpha() or len(guess_char) > 1:
+            raise ValueError('Invalid guess')            
+        if guess_char in self._word:
+            self._replace_guess_char(guess_char)
+        else:
+            self.num_guess -= 1
+            match_found = False
+        return match_found
+    
+    def _replace_guess_char(self, guess_char):
+        start = 0
+        while True:
+            index = self._word.find(guess_char, start)
+            if index == -1:
+                break
+            else:
+                self._lst_guess[index] = guess_char
+                start  = index + 1
+                if '_' not in self._lst_guess:
+                    self.num_guess = -1
+                    
+    def get_result(self):
+        return self.num_guess == -1
+    
 
 def main():
-    word = words[random.randint(0, 4)]
-    num_guess = 6
-    lst_guess = ['_' for i in range(0, len(word))]
-    
-    while (num_guess > 0):
-        print('Guesses left {}\n'.format(num_guess))
-        print(' '.join(lst_guess))
+    hg = hangman()
+       
+    while (hg.guess_remain() > 0):
+        print('Guesses left {}\n'.format(hg.num_guess))
+        print(' '.join(hg.lst_guess))
         guess_char = input('Enter your guess:')
-        if not guess_char.isalpha() or len(guess_char) > 1:
+        try:
+            if hg.process_guess(guess_char) == False:
+                print ('Incorrect guess!!!')
+                input('Press any key to continue.')
+        except ValueError:
             print('Please enter only one alaphabet as your guess.')
             input('Press any key to continue.')
-            continue
-        if guess_char in word:
-            start = 0
-            while True:
-                index = word.find(guess_char, start)
-                if index == -1:
-                    break
-                else:
-                    lst_guess[index] = guess_char
-                    start = index + 1
-                    if '_' not in lst_guess:
-                        num_guess = -1
-        else:
-            print ('Incorrect guess!!!')
-            input('Press any key to continue.')
-            num_guess-= 1
             
-    if num_guess == 0:
-        print('You Lose!!!')
-        print('The correct word was {}'. format(word))
+    if hg.get_result():
+        print('You WIN!!!')       
     else:
-        print('You WIN!!!')
+        print('You Lose!!!')
+        print('The correct word was {}'. format(hg.guess_word))
+        
  
 
 if __name__ == "__main__":

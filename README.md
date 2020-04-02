8bc26284370ad61c7bf21dfac79aef57efaa0646
Roland Fernandes <roland.fernandes@gmail.com>
2020-04-01 21:15:32 -0400

    Modified the GUI to use AppJar library

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # games
 Repository to hold games and related code developed in Python
+
+Hangman:
+The classic game of Hangman. Runs on terminal window. No GUI. GUI planned for 
+future.
+
+RPS:
+The classic game of Rock Paper Scissors. 
+GUI uses the AppJar library. AppJar is a simple & intuitive wrapper for Tkinter. More information can be found at 
+
+http://appjar.info/
+
+AppJar can be installed using pip or can be downloaded from the above link
--- rps/main.py
+++ rps/main.py
@@ -8,7 +8,7 @@ Created on Sun Feb 23 14:18:56 2020
 
 import random
 import tkinter as tk
-
+from appJar import gui
 
 def process_user_choice(user_choice):
     rock_result_list = [    
@@ -41,75 +41,62 @@ def process_user_choice(user_choice):
     comp_choice_lst[comp_choice]
     
    
-class MainApp():
+class MainAppJar():
     
-    _modes = [
-        ('Rock', 'Rock'),
-        ('Paper', 'Paper'),
-        ("Scissor", 'Scissor')
-    ]
+    _choices = [ 'Rock', 'Paper', 'Scissor']
     
-    def _create_main_window(self):
-        self._main_window = tk.Tk()
-        self._main_window.title('Rock Paper Scissor')
-        self._main_window.resizable
-        
-        
-    def _create_frame(self):
-        self._frame = tk.Frame(self._main_window)
-        self._frame.pack()
-        
-        
+    def _init_gui(self):
+        self._app = gui('Rock Paper Scissor', '400X400')
+        self._app.setBg('slategrey')
+        self._app.setFont(18)
+    
+    
     def _create_radio_choice(self):
-        choice_label = tk.Label(self._frame, text="Select your choice:",
-                                fg='Green')
-        choice_label.pack()        
+        self._app.addLabel('lbl_choice', 'Select your choice:')
+        self._app.setLabelBg('lbl_choice', 'blue')
+        self._app.setLabelFg('lbl_choice', 'orange')
         
-        self._var_choice = tk.StringVar()
-        self._var_choice.set("Rock") # initialize
+        for choice in self._choices:
+            self._app.addRadioButton('rad_choice', choice)
+        self._app.setRadioButton('rad_choice', self._choices[0], False)
+        self._app.setRadioButtonChangeFunction('rad_choice', self._sel_change)
         
-        for text, mode in self._modes:
-            rad_choice = tk.Radiobutton(self._frame, text=text,
-                    variable=self._var_choice, value=mode)
-            rad_choice.pack(anchor=tk.W)
-            
-            
+        
     def _create_lbl_status(self):
-        self._lbl_comp = tk.Label(self._main_window, text="", width=25,
-                                  height=10, fg='Green')
-        self._lbl_comp.pack()
-        
-        self._lbl_result = tk.Label(self._main_window, text="", width=10,
-                                    height=10, fg='Green')
-        self._lbl_result.pack()
+       self._app.addLabel('lbl_comp_choice', '')
+       self._app.addLabel('lbl_result_choice', '')
     
     
     def _create_btn_ctrl(self):
-        self._btn_play = tk.Button(self._main_window, text='Play', width=25,
-                                   command=self._play_clicked)
-        self._btn_play.pack(side=tk.LEFT)
-        self._btn_quit = tk.Button(self._main_window, text='Quit', width=25,
-                                   command=self._main_window.destroy)
-        self._btn_quit.pack(side=tk.LEFT)
-        
+         self._app.addButtons(['Play', 'Quit'], self._btn_clicked)
+         
     
-    def _play_clicked(self):
-        result, comp_choice = process_user_choice(self._var_choice.get())
-        self._lbl_comp['text'] = 'The computer chooses {0}'.format(comp_choice)
-        self._lbl_result['text'] = 'You {0}'.format(result)
+    def _btn_clicked(self, button):
+        if button == 'Quit':
+            self._app.stop()
+        else:
+            result, comp_choice = process_user_choice(self._app. \
+                                getRadioButton('rad_choice'))
+            self._app.setLabel('lbl_comp_choice', 'The computer chooses {0}' \
+                               .format(comp_choice))
+            self._app.setLabel('lbl_result_choice', 'You {0}'.format(result))
+            
+    def _sel_change(self):
+        self._app.setLabel('lbl_comp_choice', '')
+        self._app.setLabel('lbl_result_choice', '')
+            
         
-                
     def __init__(self):
-        self._create_main_window()
-        self._create_frame()
+        self._init_gui()
         self._create_radio_choice()
         self._create_lbl_status()
         self._create_btn_ctrl()
-        self._main_window.mainloop()
+        self._app.go()
         
         
         
 if __name__ == "__main__":
-    app = MainApp()
+    app = MainAppJar()
+    
     
 

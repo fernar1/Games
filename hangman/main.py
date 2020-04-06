c738181e40501874fd61abb72412fd4b28b48f79
Roland Fernandes <roland.fernandes@gmail.com>
2020-04-05 23:44:24 -0400

    Added hangaman images to the GUI

--- hangman/main.py
+++ hangman/main.py
@@ -8,7 +8,8 @@ Created on Sun Mar 15 14:24:14 2020
 
 from appJar import gui
 from hangman import Hangman
- #-----------------------------------------------------------------------------
+
+ #--------------------------Begin Class MainAppJar-----------------------------
    
 class MainAppJar():
     """
@@ -21,12 +22,24 @@ class MainAppJar():
             ['S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
     )
     
+    _tup_images = ('1.gif', '2.gif', '3.gif', '4.gif', '5.gif', '6.gif', \
+                   '7.gif')
+    
+    
+    def _enable_disable_char_btns(self, enable=True):
+        for lst_char_line in self._tup_char_lists:
+            for char in lst_char_line:
+                if enable:
+                    self._app.enableButton(char)
+                else:
+                    self._app.disableButton(char)
+        
     
     def _display_result(self):
         if self._hg.get_result():
-            self._app.setLabel('lbl_guess', 'You Win!!!')
+            self._app.setLabel('lbl_result', 'You Win!!!')
         else:
-            self._app.setLabel('lbl_guess', 'You Lose!!!')
+            self._app.setLabel('lbl_result', 'You Lose!!!')
             self._app.setLabel('lbl_word', self._hg.guess_word)
     
     
@@ -34,40 +47,44 @@ class MainAppJar():
         if self._hg.process_guess(button.lower()) == True:
             self._app.setLabel('lbl_word', ' '.join(self._hg.lst_guess))
         else:
-            self._app.setLabel('lbl_guess', 'Guesses left:{0}' \
-                           .format('*' * self._hg.num_guess))
+            self._app.setImage('img_hangman', self. \
+                               _tup_images[self._hg.num_guess])
         
         self._app.disableButton(button)
         
         if self._hg.guess_remain() < 1:
-          self._display_result()                         
+            self._display_result() 
+            self._enable_disable_char_btns(False)
                            
      
     def _btn_ctrls_clicked(self, button): 
         if button == 'New Word':
             self._hg.init_data()
             self._app.setLabel('lbl_word', ' '.join(self._hg.lst_guess))
-            self._app.setLabel('lbl_guess', 'Guesses left:{0}' \
-                           .format('*' * self._hg.num_guess))
-            for lst_char_line in self._tup_char_lists:
-                for char in lst_char_line:
-                    self._app.enableButton(char)
+            self._app.setImage('img_hangman', self._tup_images[6])
+            self._app.setLabel('lbl_result', '')
+            self._enable_disable_char_btns(True)
         else:
             self._app.stop()
             
         
     def _init_gui(self):
-        self._app = gui('Hangman', '400x400')
+        self._app = gui('Hangman', '600x600')
         self._app.setBg('slategrey')
         self._app.setFont(12)
-        self._app.addLabel('lbl_guess', 'Guesses left:{0}' \
-                           .format('*' * self._hg.num_guess))
-        self._app.addLabel('lbl_word', ' '.join(self._hg.lst_guess))
+        
+        self._app.setImageLocation('./images')
+        self._app.addImage('img_hangman', self._tup_images[6])
+        
+        self._app.addLabel('lbl_result', '')
+        
+        self._app.addLabel('lbl_word', ' '.join(self._hg.lst_guess)). \
+            config(font='Helvetica 40')
         
         for lst_char_line in self._tup_char_lists:
             self._app.addButtons(lst_char_line, self._word_sel)
 
-        self._app.addLabel('lbl_sep', '-' * 96)
+        self._app.addHorizontalSeparator(colour='black')
         self._app.buttons(['New Word', 'Quit'], self._btn_ctrls_clicked, \
                           width=20)
         
@@ -77,7 +94,9 @@ class MainAppJar():
         self._init_gui()
         self._app.go()
         
-    
-#-----------------------------------------------------------------------------    
+
+#---------------------------End Class MainAppJar-------------------------------
+        
 if __name__ == "__main__":
-    MainAppJar()
+    app = MainAppJar()
+ 
